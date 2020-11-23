6aad5e3e7d7582b7a3875c95ff81f4b8fb76e0e0
Chris Mann <chrismann809@gmail.com>
2020-11-23 15:21:18 -0500

    finished hashing

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -5,15 +5,20 @@ end
 class Array
   def hash
     self << 1 if self.empty?
-    h = self.map {|ele| (ele ** self.index(ele)).hash}
+    h = self.map {|ele| (ele * self.index(ele)).hash}
     h.inject {|acc, ele| acc ^ ele}
   end
 end
 
 class String
   def hash
-    self.split("").map{|ele| ele.to_i}
-
+    # self.split("").map{|ele| ele.to_i}
+    # chars.map {|c| c.succ }.hash
+    h = []
+    self.each_byte do |char|
+      h << char
+    end
+    h.hash
   end
 end
 
@@ -21,8 +26,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    k_str = self.keys.sort.join("")
+    v_str = self.values.sort.join("")
+    k_str.hash + v_str.hash
   end
 end
-
-p ("Hello").hash
